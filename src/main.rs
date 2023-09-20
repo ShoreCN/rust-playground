f92d8481f29899ec793f386538129f60ec1fe668
Shr <sh66123457@gmail.com>
2023-09-20 14:37:56 +0800

    feat: var shadowing

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,15 @@ fn main() {
     let chinese = "你好，世界！";
     let japanese = "こんにちは世界";
     let korean = "안녕하세요 세계";
-    for region in [chinese, japanese, korean] {
+    let regions = [chinese, japanese, korean];
+    let len = regions.len();
+
+    println!("Iterate the regions array, len = {}", len);
+    for region in regions {
         println!("{}", region);
     }
+
+    // 变量遮蔽, 改变变量的内容
+    let len = 0;
+    println!("Iterate over, clear regions, len = {}", len);
 }
