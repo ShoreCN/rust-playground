74c3567bb305ec046ffd57b43b055d6c9d283e31
Shr <cs06@163.com>
2023-10-22 03:35:26 +0800

    FnOnce

--- src/closure.rs
+++ src/closure.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 
 
 struct Cacher<T>
@@ -42,6 +44,26 @@ fn fn_mut_closure() {
     // 无法通过编译, 因为没有声明mut, 闭包默认是不可变的
     // let add_num2 = |x: u32| num += x;
     // add_num2(5);
+
+    let c = |x| x;
+    let c5 = c(5);
+    let c7 = c(7);
+    println!("c(5) = {c5}, c(7) = {c7}");
+
+    let p1: u32 = 10;
+    let fn_once = move |x: u32| x;
+    let fn_once5 = fn_once(p1);
+    // 可以通过编译, 因为闭包已经move了p1的所有权, 但是p1的类型是u32, 实现了Copy trait, 所以可以继续使用
+    let fn_once7 = fn_once(p1);
+    println!("fn_once(5) = {fn_once5}, fn_once(7) = {fn_once7}");
+    let p2 = vec![1, 2, 3];
+    let fn_once2 = move |x| x;
+    let fn_once2_1 = fn_once2(p2);
+    // 无法通过编译, 因为闭包已经move了p2的所有权, 所以无法继续使用
+    // let fn_once2_2 = fn_once2(p2);
+    println!("fn_once2_1 = {:?}", fn_once2_1);
+    // println!("p2 = {:?}", p2);
+
 }
 
 fn cacher_test() {
