5f5def4794b2f505e6685388fd7b1383545035d5
Shr <cs06@163.com>
2023-11-13 16:09:28 +0800

    thread msg channel

--- src/threads.rs
+++ src/threads.rs
@@ -1,14 +1,14 @@
 use std::thread;
 use crate::elevator::{Elevator, State};
 use rand;
-use std::sync::{Arc, Barrier};
+use std::sync::{Arc, Barrier, mpsc};
 use std::cell::RefCell;
 
 // 线程局部变量
 thread_local!(static INIT_WEIGHT: RefCell<u32> = RefCell::new(1000));
 
 impl Elevator {
-    fn goto_random_floor(&mut self, elevator_num: u8) {
+    fn goto_random_floor(&mut self, elevator_num: u8) -> Result<String, &'static str> {
         println!("elevator[{elevator_num}] current floor is {}", self.current_floor);
         let random_floor = rand::random::<u8>() % 33 + 1;
         self.current_floor = random_floor;
@@ -16,7 +16,8 @@ impl Elevator {
         INIT_WEIGHT.with(|weight| {
             self.current_weight = *weight.borrow() + elevator_num as u32 * 100;
         });
-        println!("elevator[{elevator_num}] go to floor {}", self.current_floor);
+        println!("elevator[{elevator_num}] go to floor {}", random_floor);
+        Ok(format!("Nofitication: Elevator[{}] arrived {}.", elevator_num, random_floor))
     }
 }
 
@@ -32,11 +33,20 @@ fn elevators_process(){
         println!("modified weight = {}", *weight.borrow());
     });
 
+    // 创建消息通道
+    let (tx, rx) = mpsc::channel();
+    tx.send("Notification init".to_string()).unwrap();
+
     for i in 0..threads_num as u8 {
         let thread_barrier = barrier.clone();
+        let multi_tx = tx.clone();
+        let mut elevator = Elevator::new();
         let handler = thread::spawn(move || {
-            let mut elevator = Elevator::new();
-            elevator.goto_random_floor(i);
+            let r = elevator.goto_random_floor(i);
+            // 如果goto_random_floor()返回的是Ok, 则说明电梯已经到达指定楼层, 通过消息通道通知主线程
+            if let Ok(msg) = r {
+                multi_tx.send(msg).unwrap();
+            }
             // 为了让所有的电梯状态打印都在最后, 使用线程屏障控制线程执行到此处进行等待
             thread_barrier.wait();
             println!("elevator[{i}] state is {:?}, weight is {}", elevator.state(), elevator.current_weight);
@@ -47,6 +57,15 @@ fn elevators_process(){
     // 等待所有线程结束
     for handler in threads {
         handler.join().unwrap();
+    }
+
+    // 所有的tx都drop掉之后, 使用for循环接收消息通道中的消息才会结束
+    // tx的clone在线程中会被drop掉, 所以这里只需要drop掉主线程中的tx即可
+    drop(tx);
+
+    // 接收消息通道中的消息
+    for received in rx {
+        println!("Message received >> {}", received);
     }
 
     // 子线程使用完线程局部变量之后再查看一下线程局部变量的值
