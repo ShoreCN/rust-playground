19feeb1d02735591cbf049e841fd0d38c844cc9f
Shr <cs06@163.com>
2023-11-22 23:03:32 +0800

    Atomic usage

--- src/threads.rs
+++ src/threads.rs
@@ -231,10 +231,51 @@ fn condvar_usage() {
     handler.join().unwrap();
 }
 
+use std::ops::Sub;
+use std::sync::atomic::{AtomicU64, Ordering};
+use std::time::Instant;
+
+const N_TIMES: u64 = 10000000;
+const N_THREADS: usize = 10;
+
+static R: AtomicU64 = AtomicU64::new(0);
+// static M: Mutex<u64> = Mutex::new(0);
+
+fn add_n_times(n: u64) -> thread::JoinHandle<()> {
+    thread::spawn(move || {
+        for _ in 0..n {
+            R.fetch_add(1, Ordering::Relaxed);
+
+            // 使用Mutex也可以实现原子操作, 不过性能会比AtomicU64差很多
+            // let mut m = M.lock().unwrap();
+            // *m += 1;
+        }
+    })
+}
+
+fn atomic_test() {
+    let s = Instant::now();
+    let mut threads = Vec::with_capacity(N_THREADS);
+
+    for _ in 0..N_THREADS {
+        threads.push(add_n_times(N_TIMES));
+    }
+
+    for thread in threads {
+        thread.join().unwrap();
+    }
+
+    assert_eq!(N_TIMES * N_THREADS as u64, R.load(Ordering::Relaxed));
+    // assert_eq!(N_TIMES * N_THREADS as u64, *M.lock().unwrap());
+
+    println!("{:?}",Instant::now().sub(s));
+}
+
 pub fn threads() {
     elevators_process();
     thread_sync_msg();
     different_data_send();
     rwlock_test();
     condvar_usage();
+    atomic_test();
 }
