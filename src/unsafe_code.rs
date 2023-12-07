e5f8b54268e4b0059a030ef2625caec24acf943b
Shr <cs06@163.com>
2023-12-07 15:14:23 +0800

    str content as raw pointer

--- src/unsafe_code.rs
+++ src/unsafe_code.rs
@@ -1,4 +1,5 @@
-
+use std::slice::from_raw_parts;
+use std::str::from_utf8;
 
 fn raw_pointer() {
     let mut x = 1;
@@ -10,6 +11,18 @@ fn raw_pointer() {
         *mut_raw_ptr = 2;
         println!("mut_raw_ptr = {}", *mut_raw_ptr);
         println!("const_raw_ptr = {}", *const_raw_ptr);
+    }
+
+    let s = "This string is to test raw pointer";
+    let s_raw_ptr = s.as_ptr();
+    let s_len = s.len();
+    unsafe {
+        // print the content of s_ptr
+        let s_ptr = from_raw_parts(s_raw_ptr, s_len);
+        println!("s_ptr = {:?}", s_ptr);
+        // print the content of s_ptr as string
+        let s_str = from_utf8(s_ptr).unwrap();
+        println!("s_str = {}", s_str);
     }
 }
 
