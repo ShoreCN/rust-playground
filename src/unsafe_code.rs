a1ed353578788197806b6cfe9d7810d50e613a84
Shr <cs06@163.com>
2023-12-07 16:12:25 +0800

    raw pointer of str

--- src/unsafe_code.rs
+++ src/unsafe_code.rs
@@ -13,6 +13,7 @@ fn raw_pointer() {
         println!("const_raw_ptr = {}", *const_raw_ptr);
     }
 
+    // 通过裸指针加上长度, 可以访问内存中的一段str数据
     let s = "This string is to test raw pointer";
     let s_raw_ptr = s.as_ptr();
     let s_len = s.len();
@@ -24,6 +25,14 @@ fn raw_pointer() {
         let s_str = from_utf8(s_ptr).unwrap();
         println!("s_str = {}", s_str);
     }
+
+    // 也可以通过str的引用获取str的裸指针, 直接解引用就可以得到str
+    let s = "This string is to test raw pointer";
+    let s_raw_ptr = &s as *const &str;
+    unsafe {
+        println!("s_raw_ptr = {}", *s_raw_ptr);
+    }
+
 }
 
 
